c0a6925252ccd814352aa8f781f0d69dabecb71e
Efren Cavazos <efrencavazosimac@Efrens-iMac.attlocal.net>
2018-09-03 14:27:53 -0500

    debugging

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,47 @@
 $(document).ready(function(){
 // Variables
-    var random = Math.floor(Math.random() * 101 + 19) 
-    var ametist = Math.floor(Math.random() * 11 + 1)
-    var emerald = Math.floor(Math.random() * 11 + 1)
-    var rubin = Math.floor(Math.random() * 11 + 1)
-    var safir = Math.floor(Math.random() * 11 + 1)
+    var random = Math.floor(Math.random() * 101 + 19); 
+    var ametist = Math.floor(Math.random() * 11 + 1);
+    var emerald = Math.floor(Math.random() * 11 + 1);
+    var rubin = Math.floor(Math.random() * 11 + 1);
+    var safir = Math.floor(Math.random() * 11 + 1);
     var userTotal = 0;
     var wins = 0;
     var losses = 0;
 
-    var newChest = new Array() 
+    var newChest = new Array(); 
     // Random images
-    newChest[0] = "../images/spiked_chest_small.png"
-    newChest[1] = "../images/common_chest.png"
-    newChest[2] = "../images/crystal_chest.png"
-    newChest[3] = "../images/nefrit_chest.png"
-    newChest[4] = "../images/red_chest.png"
+    newChest[0] = "http://www.efren-cavazos.com/unit-4-game/assets/images/spiked_chest_small.png";
+    newChest[1] = "http://www.efren-cavazos.com/unit-4-game/assets/images/common_chest.png";
+    newChest[2] = "http://www.efren-cavazos.com/unit-4-game/assets/images/crystal_chest.png";
+    newChest[3] = "http://www.efren-cavazos.com/unit-4-game/assets/images/nefrit_chest.png";
+    newChest[4] = "http://www.efren-cavazos.com/unit-4-game/assets/images/red_chest.png";
 
     var j = 0;
     var p = newChest.length;
-    var preBuffer = new Array()
+    var preBuffer = new Array();
 
     for (i = 0; i < p; i++) {
-        preBuffer[i] = new Image()
-        preBuffer[i].src = newChest[i]
+        preBuffer[i] = new Image();
+        preBuffer[i].src = newChest[i];
     }
     var whatImage = Math.floor(Math.random() * (p-1));
-
+});
     function showImage() {
         if(whatImage === 0) {
-            document.write('<img src="'newChest[whatImage]'"></a>');
+            document.write('<img src="'+newChest[whatImage]+'" border=0 width=283 </a>');
         }
         else if(whatImage === 1) {
-            document.write('<img src="'newChest[whatImage]'"></a>');
+            document.write('<img src="'+newChest[whatImage]+'" border=0 width=283 </a>');
         }
         else if(whatImage === 2) {
-            document.write('<img src="'newChest[whatImage]'"></a>');
+            document.write('<img src="'+newChest[whatImage]+'" border=0 width=283 </a>');
         }
         else if(whatImage === 3) {
-            document.write('<img src="'newChest[whatImage]'"></a>');
+            document.write('<img src="'+newChest[whatImage]+'" border=0 width=283 </a>');
         }
         else if(whatImage === 4) {
-            document.write('<img src="'newChest[whatImage]'"></a>');
+            document.write('<img src="'+newChest[whatImage]+'" border=0 width=283 </a>');
         }
     }
     
@@ -55,7 +55,7 @@ $(document).ready(function(){
 // Resets the game
     function reset() {
         random = Math.floor(Math.random() * 101 + 19);
-        console.log(random)
+        console.log(random);
         $("#randomNumber").text(random);
         var ametist = Math.floor(Math.random() * 11 + 1);
         var emerald = Math.floor(Math.random() * 11 + 1);
@@ -63,21 +63,21 @@ $(document).ready(function(){
         var safir = Math.floor(Math.random() * 11 + 1);
         userTotal = 0;
         $("#finalTotal").text(userTotal);
-    }
+    };
 // Win Function
     function youWin() {
         alert("You Won!");
         wins++;
         $("#numberWins").text(wins);
         reset();
-    }
+    };
 // Lose Function
     function youLose() {
         alert ("You Lose!");
         losses++;
         $("#numberLosses").text(losses);
         reset();
-    }
+    };
 
 // On Click Options
 // Ametist
@@ -91,7 +91,7 @@ $(document).ready(function(){
         else if(userTotal > random) {
             youLose();
         }
-    })
+    });
 
 // Emerald
     $("#emerald").on("click", function(){
@@ -131,4 +131,3 @@ $(document).ready(function(){
             youLose();
         }
     });
-});
