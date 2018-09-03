70556f1c4274fa8809728bdd1180acdd36aec5b0
Efren Cavazos <efrencavazosimac@Efrens-iMac.attlocal.net>
2018-09-03 11:18:26 -0500

    updated javascript and css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,93 +1,87 @@
-//Global variables that are accesible for all
-var counter = 0;
-var wins = 0;
-var losses = 0;
-//wins and losses counter
-$("#win").text(wins);
-$("#losses").text(losses);
-//Players current score
-var addition = 0;
+$(document).ready(function(){
+    var random = Math.floor(Math.random() * 50) 
+    $("#randomNumber").text(random);
 
-//Selecting a random number
-var total = Math.floor(Math.random() * 10 + 1);
-$(".total").empty();
-$(".total").append(total);
+    var ametist = Math.floor(Math.random() * 11)
+    var emerald = Math.floor(Math.random() * 11)
+    var rubin = Math.floor(Math.random() * 11)
+    var safir = Math.floor(Math.random() * 11)
 
-//Assign a random number to each crystals
-var red = Math.floor(Math.random() * 100 + 1);
-var blue = Math.floor(Math.random() * 100 + 1);
-var green = Math.floor(Math.random() * 100 + 1);
-var yellow = Math.floor(Math.random() * 100 + 1);
+    var userTotal = 0;
+    var wins = 0;
+    var losses = 0;
 
-//Random image
-var myPix = new Array("../images/red.png", "../images/blue.png", "../images/green.png", "../images/yellow.png");
-var randomNum = Math.floor(Math.random() * myPix.length);
-for(var i = 0; i < myPix.length; i++) {
-    document.slide.src = myPix[i];
-}
+    $("#numberWins").text(wins);
+    $("#numberLosses").text(losses);
+    $("#finalTotal").text(userTotal);
 
-document.getElementById("myPicture").src = myPix[randomNum];
+    function reset() {
+        random = Math.floor(Math.random() * 11);
+        console.log(random)
+        $("#randomNumber").text(random);
+        var ametist = Math.floor(Math.random() * 11);
+        var emerald = Math.floor(Math.random() * 11);
+        var rubin = Math.floor(Math.random() * 11);
+        var safir = Math.floor(Math.random() * 11);
+        userTotal = 0;
+        $("#finalTotal").text(userTotal);
+    }
 
-//Appending the addition in the HTML
-var updateAddition = function(){
-        $(".addition").empty();
-        $(".addition").append(addition);
-        $("#wins").empty();
-        $(".#wins").append(wins);
-        $("#losses").empty();
-        $("#losses").append(losses);
-}
-
-//Restarting the function can be achieved as follow
-var restart = function(){
-        addition = 0;
-        total = Math.floor(Math.random()* 100 + 1);
-        $(".total").empty();
-        $(".total").append(total);
-        red = Math.floor(Math.random() * 100 + 1);
-        blue = Math.floor(Math.random() * 100 + 1);
-        green = Math.floor(Math.random() * 100 + 1);
-        yellow = Math.floor(Math.random() * 100 + 1);
-        updateAddition();
-}
-
-//Wins and losses
-var winsLosses = function(){
-        if(addition === total) {
-            wins++;
-            alert("Yeah, Motha Fucka! YOU WIN!!!");
-            restart();
-        } else if(addition > total){
-            losses++;
-            alert("Bitch, YOU SUCK!!!");
-            restart();
-        }else {
-            updateAddition();
+    function youWin() {
+        alert("You Won!");
+        wins++;
+        $("#numberWins").text(wins);
+        reset();
+    }
+    function youLose() {
+        alert ("You Lose!");
+        losses++;
+        $("#numberLosses").text(losses);
+        reset();
+    }
+    $("#ametist").on("click", function(){
+        userTotal = userTotal + ametist;
+        console.log("New userTotal = " + userTotal);
+        $("finalTotal").text(userTotal);
+        if(userTotal === random) {
+            youWin();
         }
-}
-$(".addition").append(total);
-$(".addition").append(addition);
-
-//The onclick
-$(document).ready(function(){
-    $("#red").click(function(){
-        addition + red;
-        winsLosses();
+        else if(usertTotal > random) {
+            youLose();
+        }
     })
-$(document).ready(function(){
-    $("#blue").click(function(){
-        addition + blue;
-        winsLosses();
+    $("#emerald").on("click", function(){
+        userTotal = userTotal + emerald;
+        console.log("New userTotal = " + userTotal);
+        $("finalTotal").text(userTotal);
+        if(userTotal === random) {
+            youWin();
+        }
+        else if(usertTotal > random) {
+            youLose();
+        }
     })
-$(document).ready(function(){
-    $("#green").click(function(){
-        addition + green;
-        winsLosses();
+    $("#safir").on("click", function(){
+        userTotal = userTotal + safir;
+        console.log("New userTotal = " + userTotal);
+        $("finalTotal").text(userTotal);
+        if(userTotal === random) {
+            youWin();
+        }
+        else if(usertTotal > random) {
+            youLose();
+        }
     })
-$(document).ready(function(){
-    $("#yellow").click(function(){
-        addition + yellow;
-        winsLosses();
-    })     
+    $("#rubin").on("click", function(){
+        userTotal = userTotal + rubin;
+        console.log("New userTotal = " + userTotal);
+        $("finalTotal").text(userTotal);
+        if(userTotal === random) {
+            youWin();
+        }
+        else if(usertTotal > random) {
+            youLose();
+        }
+    });
 
 });
